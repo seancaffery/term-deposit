e648d4fc4bd5a5f516d447029b052e741e0f6eba
Sean Caffery <sean@lineonpoint.com>
2025-06-26 17:07:25 +1000

    Simplify totalBalance test setup

--- balance_calculator_test.go
+++ balance_calculator_test.go
@@ -6,50 +6,38 @@ import (
 )
 
 func Test_totalBalance(t *testing.T) {
+	startAmount := 10000
+	interestRate := 2.1
+	term := 5
 	tests := []struct {
 		name            string
-		startAmount     float64
-		interestRate    float64
-		term            int
 		interestPayment interestPeriod
 		expectedBalance float64
 	}{
 		{
 			name:            "interest paid at maturity",
-			startAmount:     10000,
-			interestRate:    2.1,
-			term:            5,
 			interestPayment: maturity,
 			expectedBalance: 11050,
 		},
 		{
 			name:            "interest paid yearly",
-			startAmount:     10000,
-			interestRate:    2.1,
-			term:            5,
 			interestPayment: yearly,
 			expectedBalance: 11095,
 		},
 		{
 			name:            "interest paid monthly",
-			startAmount:     10000,
-			interestRate:    2.1,
-			term:            5,
 			interestPayment: monthly,
 			expectedBalance: 11106,
 		},
 		{
 			name:            "interest paid quarterly",
-			startAmount:     10000,
-			interestRate:    2.1,
-			term:            5,
 			interestPayment: quarterly,
 			expectedBalance: 11104,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := totalBalance(tt.startAmount, tt.interestRate, tt.term, tt.interestPayment)
+			got := totalBalance(float64(startAmount), interestRate, term, tt.interestPayment)
 
 			assert.Equal(t, tt.expectedBalance, got)
 		})
